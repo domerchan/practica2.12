c14b3dd2b92b17a4b6ff718bde27fc865f91efd6
Do <Estudiante@lab0408>
2017-12-08 10:27:09 -0500

    actualizacion

--- PAp_Ejercicio12/src/vista/A.java
+++ PAp_Ejercicio12/src/vista/A.java
@@ -1,11 +1,15 @@
 package vista;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.swing.BorderFactory;
 import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
 import controlador.GestionDatos;
 
@@ -16,7 +20,18 @@ public class A extends JInternalFrame implements ActionListener {
 	public A(GestionDatos gd) {
 		
 		Locale localizacion = VentanaGrafica.localizacion;
-		ResourceBundle lang = ResourceBundle.getBundle("lang.mesajes", localizacion);
+		ResourceBundle lang = ResourceBundle.getBundle("Idioma.mesajes", localizacion);
+		
+		this.gd = gd;
+		setSize(750, 209);
+		
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+		
+		JPanel pnlDatos = new JPanel();
+		pnlDatos.setBorder(BorderFactory.createTitledBorder(lang.getString("Datos")));
+		
+		
 	}
 	
 	@Override
@@ -24,5 +39,13 @@ public class A extends JInternalFrame implements ActionListener {
 		
 	}
 
-	
+	/*ESPAÑOL
+	 Datos Datos
+	 
+	 
+	 *INGLES
+	 Datos Data
+	 
+	 
+	 */
 }
--- PAp_Ejercicio12/src/vista/C.java
+++ PAp_Ejercicio12/src/vista/C.java
@@ -1,20 +1,42 @@
 package vista;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+import javax.swing.BorderFactory;
 import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
 import controlador.GestionDatos;
 
 public class C extends JInternalFrame implements ActionListener {
 
+	private GestionDatos gd;
+	
+	public C(GestionDatos gd) {
 
+		Locale localizacion = VentanaGrafica.localizacion;
+		ResourceBundle lang = ResourceBundle.getBundle("Idioma.mesajes", localizacion);
+
+		this.gd = gd;
+		setSize(750, 209);
+
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout());
+
+		JPanel pnlDatos = new JPanel();
+		pnlDatos.setBorder(BorderFactory.createTitledBorder(lang.getString("Datos")));
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
